88d44139ca3adbdbea1c413c59e280f2a7a2ec6b
Akaiiii 65 <http://github.com/akashaj5455-jpg>
2026-05-17 13:25:48 +0530

    Hardcoded Google Client ID for Render build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID';
+const GOOGLE_CLIENT_ID = '911421324448-csm8c7c5g6scabf12dfbs0mc5oi0e33q.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
